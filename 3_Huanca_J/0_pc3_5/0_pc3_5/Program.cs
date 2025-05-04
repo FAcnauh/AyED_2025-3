066abc20a9ccccd01a982bbf632acb3933fa5350
FAcnauh <Facundomartinezhuanca@gmail.com>
2025-05-03 22:14:01 -0300

    0_PC3_5

---
+++ 3_Huanca_J/0_pc3_5/0_pc3_5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_pc3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             Escribe un programa en C# que calcule el resultado de restar dos números introducidos por el usuario.
+             */
+
+            int num1;
+            int num2;
+            int numf;
+
+            Console.WriteLine("numero 1:");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("numero 1 es " + num1);
+
+
+            Console.WriteLine("numero 2:");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("numero 2 es " + num2);
+
+            numf = num1 - num2;
+
+            Console.WriteLine("resultado de la resta es " + numf);
+
+            Console.ReadKey();
+
+        }
+    }
+}
