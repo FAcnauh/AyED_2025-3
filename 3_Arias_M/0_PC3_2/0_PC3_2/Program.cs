55a69630a01bbee2f059e7bbb6684dc9903427b8
RAyasillac <ariasrashid5@gmail.com>
2025-05-06 21:29:23 -0300

    0_PC3_2

---
+++ 3_Arias_M/0_PC3_2/0_PC3_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_PC3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            TP 2 - Etapa 0
+            Desarrollar un codigo en el que amcenemos el nombre en una variable "nombre"y la edad en la variable "edad" Finalmente debe saludar "hola NOMBRE", tu edad es EDAD"
+            */
+
+            //Declaramos variables
+            string nombre;
+            int edad;
+
+            //Instanciamos
+            nombre = "Facundo Huanca";
+            edad = 15;
+
+            //Imprimimos
+            Console.WriteLine("Hola" + nombre + " tu edad es: " + edad);
+
+            Console.ReadLine();
+        }
+    }
+}
