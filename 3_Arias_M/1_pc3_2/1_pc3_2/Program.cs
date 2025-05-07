e99fda3384a588030761e40b8297d0746e62be88
RAyasillac <ariasrashid5@gmail.com>
2025-05-06 21:33:45 -0300

    1_PC3_2

---
+++ 3_Arias_M/1_pc3_2/1_pc3_2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_pc3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Escribir un programa que pida al usuario dos números y muestre por pantalla su división.
+             Si el divisor es cero el programa debe mostrar un error.
+             */
+            double num1;
+            double num2;
+            double numf;
+
+            Console.WriteLine("numero 1:");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("numero 1 es " + num1);
+
+            Console.WriteLine("numero 2:");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("numero 2 es " + num2);
+
+            if (num2 == 0)
+            {
+                Console.WriteLine("ERROR: no se puede dividir por cero");
+            }
+            else
+            {
+                numf = num1 / num2;
+                Console.WriteLine("resultado de la division es: " + numf);
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
