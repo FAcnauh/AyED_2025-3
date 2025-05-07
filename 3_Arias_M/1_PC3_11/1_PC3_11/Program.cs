7aa945eec98ad4d01ce4e457db0590b227f00fd2
RAyasillac <ariasrashid5@gmail.com>
2025-05-06 21:37:35 -0300

    1_PC3_11

---
+++ 3_Arias_M/1_PC3_11/1_PC3_11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_PC3_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             Recorre los números del 1 al N. (N lo tiene que ingresar 
+             el usuario). Muestra solamente los números pares 
+             */
+
+            int n;
+
+            Console.WriteLine("ingrese un numero:");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
