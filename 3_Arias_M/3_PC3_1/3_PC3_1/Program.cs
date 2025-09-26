2b827d9d90296dee7576d614a5a44eb74131fc81
RAyasillac <ariasrashid5@gmail.com>
2025-09-26 19:24:18 -0300

    3_PC3_1

---
+++ 3_Arias_M/3_PC3_1/3_PC3_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_PC3_1
+{
+    class Program
+    {
+        static int Suma(int a, int b) // función que devuelve un int
+        {
+            return a + b; // devuelve el resultado
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el primer número: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo número: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int resultado = Suma(num1, num2);
+
+            Console.WriteLine("La suma es: " + resultado);
+            Console.ReadKey();
+        }
+    }
+}
