aa2a2ddb223dbb2003e68f1167b240d377a2aa4c
FAcnauh <Facundomartinezhuanca@gmail.com>
2025-09-26 20:47:15 -0300

    0_PC3_12

---
+++ 3_Huanca_J/0_PC3_12/0_PC3_12/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_PC3_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Escriba un programa en C# que solicite al usuario tres letras y los muestre al revés.
+             */
+
+            char a;
+            char b;
+            char c;
+
+            Console.WriteLine("escribi la primera letra:");
+            a = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("escribi la segunda letra:");
+            b = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("escribi la tercera letra:");
+            c = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("en orden " + a + b + c);
+
+            Console.WriteLine("en orden inverso " + c + b + a);
+
+            Console.ReadKey();
+        }
+    }
+}
