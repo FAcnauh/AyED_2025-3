fdc940a2bd104244e81908c03aa2e4df1292ec00
RAyasillac <ariasrashid5@gmail.com>
2025-09-26 19:22:24 -0300

    3_PC3_0

---
+++ 3_Arias_M/3_PC3_0/3_PC3_0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_PC3_0
+{
+    class Program
+    {
+        static string Saludo(string nombre)
+        {
+            return "Hola " + nombre;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese su nombre: ");
+            string nombreUsuario = Console.ReadLine();
+
+            string mensaje = Saludo(nombreUsuario);
+            Console.WriteLine(mensaje);
+
+            Console.ReadKey();
+        }
+    }
+}
